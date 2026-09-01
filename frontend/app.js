ec05f9c3fbce6bcbd758cba8aa88fdd1a9b87f1f
RyanDelpiero <49473977+RyanDelpiero@users.noreply.github.com>
2026-09-01 16:31:33 +0700

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,8 +1,8 @@
 // --- KONFIGURASI BACKEND API ---
 // const API_BASE_URL = 'http://localhost:5000/api/testcases';
 // const API_URL = 'https://avindhadashboard-production.up.railway.app';
-// const API_BASE_URL = "https://avindhadashboard-production.up.railway.app/api";
-const res = await fetch(`${API_BASE_URL}/testcases/General`);
+const API_BASE_URL = "https://avindhadashboard-production.up.railway.app/api/testcases/General";
+// const res = await fetch(`${API_BASE_URL}/testcases/General`);
 
 // --- AUTHENTICATION MODULE ---
 const REGISTERED_USERS = {
